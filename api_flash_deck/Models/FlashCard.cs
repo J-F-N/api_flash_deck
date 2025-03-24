029535da62eb80a89730690416e20387a4bc91fa
John Neigel <johnneigel@Johns-MacBook-Pro.local>
2025-03-23 17:03:37 -0700

    Added database

--- api_flash_deck/Models/FlashCard.cs
+++ api_flash_deck/Models/FlashCard.cs
@@ -6,6 +6,6 @@ public class FlashCard
 {
     public ObjectId Id { get; set; }
     public string UserId { get; set; }
-    public string Prompt { get; set; }git 
+    public string Prompt { get; set; } 
     public string Answer { get; set; }
 }
